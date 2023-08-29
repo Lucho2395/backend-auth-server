7911dee6f8f257ddcee80ea8e3e652f6d3784c92
Lucho <luis.salazarbartra01.ls@gmail.com>
2023-08-29 11:55:32 -0500

    Hola lucho commit

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ const app = express();
 //body-parser
 app.use( bodyParse.urlencoded ({ extended: true }) );
 
+app.get("/", (req, res) => {
+    res.status(200).send({ msg: "Hola Lucho"});
+});
+
 //Ruta
 app.use('/', require('./routes/auth'));
 
